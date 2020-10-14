e0d15340ed66b1ab696e3455742c9ebcbceb4bc3
Nada <nadaryma@gmail.com>
2020-10-14 18:28:16 +0200

    TP2 - Tentative question barycentre_v2

--- TP2/src/nuage.hpp
+++ TP2/src/nuage.hpp
@@ -22,6 +22,13 @@ template <typename TypePoint> class Nuage {
 template <typename TypePoint>
 TypePoint barycentre_v1(Nuage<TypePoint>&);
 
+template <typename TypePoint, template <typename> class Conteneur>
+TypePoint barycentre_v2(Conteneur<TypePoint>&);
+
+template <typename TypePoint, template <typename, typename = std::allocator<TypePoint>> class Conteneur>
+TypePoint barycentre_v2(Conteneur<TypePoint>&);
+
+
 
 /*----------------------IMPLEMENTATIONS----------------------*/
 
@@ -72,21 +79,66 @@ TypePoint barycentre_v1(Nuage<TypePoint> &n) {
     return bar;
 }
 
-template <>
-Polaire barycentre_v1<Polaire>(Nuage<Polaire> &n) {
-    double somme_t = 0;
-    double somme_r = 0;
+/* Specialisation pour test 4b  */
+//template <>
+//Polaire barycentre_v1<Polaire>(Nuage<Polaire> &n) {
+//    double somme_t = 0;
+//    double somme_r = 0;
+//    int size = n.size();
+//    Polaire bar = Polaire();
+//    if(size > 0) {
+//        Nuage<Polaire>::const_iterator it;
+//        for (it = n.begin(); it < n.end(); it++) {
+//            somme_t += it->getAngle();
+//            somme_r += it->getDistance();
+//        }
+//        bar.setAngle(somme_t/size);
+//        bar.setDistance(somme_r/size);
+//    }
+//    return bar;
+//}
+
+template <typename TypePoint, template <typename> class Conteneur>
+TypePoint barycentre_v2(Conteneur<TypePoint> &n) {
+    double somme_x = 0;
+    double somme_y = 0;
     int size = n.size();
-    Polaire bar = Polaire();
+    TypePoint bar = TypePoint();
     if(size > 0) {
-        Nuage<Polaire>::const_iterator it;
+        Cartesien c;
+        typename Conteneur<TypePoint>::const_iterator it;
         for (it = n.begin(); it < n.end(); it++) {
-            somme_t += it->getAngle();
-            somme_r += it->getDistance();
+            it->convertir(c);
+            somme_x += c.getX();
+            somme_y += c.getY();
         }
-        bar.setAngle(somme_t/size);
-        bar.setDistance(somme_r/size);
+        c.setX(somme_x/size);
+        c.setY(somme_y/size);
+        c.convertir(bar);
     }
     return bar;
 }
+
+template <typename TypePoint, template <typename, typename = std::allocator<TypePoint>> class Conteneur>
+TypePoint barycentre_v2(Conteneur<TypePoint> &n) {
+    double somme_x = 0;
+    double somme_y = 0;
+    int size = n.size();
+    TypePoint bar = TypePoint();
+    if(size > 0) {
+        Cartesien c;
+        typename Conteneur<TypePoint>::const_iterator it;
+        for (it = n.begin(); it < n.end(); it++) {
+            it->convertir(c);
+            somme_x += c.getX();
+            somme_y += c.getY();
+        }
+        c.setX(somme_x/size);
+        c.setY(somme_y/size);
+        c.convertir(bar);
+    }
+    return bar;
+}
+
+
 #endif
