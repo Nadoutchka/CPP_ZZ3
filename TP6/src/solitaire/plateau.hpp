281ad971edf7bda92b777ffcd7c3ebc49ae2c041
Nada <nadaryma@gmail.com>
2020-12-01 11:32:25 +0100

    Update class Pioche

--- TP6/src/solitaire/plateau.hpp
+++ TP6/src/solitaire/plateau.hpp
@@ -5,6 +5,16 @@
 #include <vector>
 #include "carte.hpp"
 
+// Pioche::rienRetourné     retourne vrai si la pioche est pleine et que la défausse est vide.
+// Pioche::toutRetourné     retourne vrai si la pioche est vide et la défausse est pleine.
+// Pioche::getVisible       retourne la carte au sommet de la défausse (la dernière qui a été piochée).
+// Pioche::tirer            déplace la carte au sommet de la pioche vers la défausse, puis retourne vrai si la pioche n'était pas vide.
+// Pioche::retirer          retire la carte au sommet de la défausse (pop) et retourne cette carte.
+// Pioche::ajouter          insère une carte face cachée dans la pioche.
+// Colonne::getCarte(i)     retourne la i-ème carte visible d'une colonne.
+// Colonne::ajouter         permettent de déplacer plusieurs cartes d'une colonne à une autre par l'intermédiaire de Main.
+// Colonne::retirer         permettent de déplacer plusieurs cartes d'une colonne à une autre par l'intermédiaire de Main.
+
 // Classe  M a i n //-------------------------------------------------------------------------------
 class Main {
  //----------------------------------------------------------------------------------------Attributs
@@ -26,13 +36,18 @@ class Colonne {
  //---------------------------------------------------------------------------------------Accesseurs
     public:
         int getTaille() const { return _colonne.size(); }
-        const Carte * getSommet() const { return *(_colonne.end()); }
+        const Carte * getSommet() const { return _colonne.back(); }
         Carte * getCarte(int pos) const { return _colonne[pos]; }
  //-----------------------------------------------------------------------------------------Methodes
         bool estVide() const { return _colonne.empty(); }
         void ajouter(Carte* carte) { _colonne.push_back(carte); }
         void ajouter(Main& m) { for (auto &it: m) ajouter(it); }
-        void retirer(int pos, Main& m);
+        void retirer(int pos, Main& m) {
+            for (auto it = _colonne.begin() + pos; it != _colonne.end(); it++) {
+                m.main.push_back(*it);
+            }
+            _colonne.erase(_colonne.begin() + pos, _colonne.end());
+        }
 };
 
 // Classe  D o c k //-------------------------------------------------------------------------------
@@ -42,13 +57,13 @@ class Dock {
         std::vector<Carte*> _dock;
  //---------------------------------------------------------------------------------------Accesseurs
     public:
-        Carte * getSommet() const { return *(_dock.end()); }
+        Carte * getSommet() const { return _dock.back(); }
  //-----------------------------------------------------------------------------------------Methodes
         bool estVide() const { return _dock.empty(); }
         void ajouter(Carte * carte) { _dock.push_back(carte); }
         Carte * retirer() {
             Carte * carte = getSommet();
-            _dock.erase(_dock.end());
+            _dock.pop_back();
             return carte;
         }
 };
@@ -58,17 +73,29 @@ class Pioche {
  //----------------------------------------------------------------------------------------Attributs
     private :
         std::vector<Carte*> _pioche;
+        std::vector<Carte*> _defausse;
  //---------------------------------------------------------------------------------------Accesseurs
     public:
-        Carte * getProchaine() const;
-        Carte * getVisible() const;
+        Carte * getProchaine() const { return _pioche.back(); };
+        Carte * getVisible() const { return _defausse.back(); };
  //-----------------------------------------------------------------------------------------Methodes
-        bool rienRetourne();
-        bool toutRetourne();
-        bool estVide() const;
-        void ajouter(Carte*);
-        bool tirer(Carte*);
-        Carte * retirer();
+        bool rienRetourne() { return !(_pioche.empty()) && _defausse.empty(); };
+        bool toutRetourne() { return !(_defausse.empty()) && _pioche.empty(); };
+        bool estVide() const { return _pioche.empty(); };
+        void ajouter(Carte * carte) { _pioche.push_back(carte); };
+        bool tirer() {
+            if (!estVide()) {
+                Carte * carte = _pioche.back();
+                _pioche.pop_back();
+                _defausse.push_back(carte);
+            }
+            return !estVide();
+        };
+        Carte * retirer() {
+            Carte * carte = _defausse.back();
+            _defausse.pop_back();
+            return carte;
+        };
 };
 
 
